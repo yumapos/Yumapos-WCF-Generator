bb156f106643631709e8a88892b2a913630f6ae5
antonioxxl <anton.sukhorukov@yumasoft.com>
2016-12-14 13:42:28 +0300

    Ignore wcf generation if not found config

--- WCF-Generator/Program.cs
+++ WCF-Generator/Program.cs
@@ -86,6 +86,11 @@ namespace WCFGenerator
             // Configure generator 
             var configs = WcfServiceClientGeneratorSettings.GetConfigs();
 
+            if (configs == null)
+            {
+                return;
+            }
+
             var wcf = new WcfGenerator(_generatorWorkspace)
             {
                 // TODO сделать возможность генерить клиенты в разные проекты
