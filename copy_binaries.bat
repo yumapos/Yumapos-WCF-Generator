6ed3271ce3f5475903ab81652470401bdf4cbcb5
avitenberg <alwitenberg@gmail.com>
2018-09-05 19:42:33 +0300

    batch changed

--- copy_binaries.bat
+++ copy_binaries.bat
@@ -1,3 +1,5 @@
+FOR %%f IN (_WCF-Generator-binaries\*.*) DO IF NOT %%f==_WCF-Generator-binaries\WCF-Generator.exe.config DEL /Q %%f
+for /d %%x in (_WCF-Generator-binaries\*) do @rd /s /q "%%x"
 xcopy WCF-Generator\bin\Debug\*.dll _WCF-Generator-binaries /s/Y
 copy WCF-Generator\bin\Debug\WCF-Generator.exe _WCF-Generator-binaries
 generation
